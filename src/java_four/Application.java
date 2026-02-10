e00d84d7022e340c43c7b95427c52158024512e3
jisoo <ekelldks@naver.com>
2026-02-10 23:45:58 +0900

    feat: 4번 수행

--- src/java_four/Application.java
+++ src/java_four/Application.java
@@ -2,6 +2,13 @@ package java_four;
 
 public class Application {
     public static void main(String[] args) {
-        Person person = new Student("홍길동", 90);
+        Person[] person = new Student[2];
+        person[0] = new Student("홍길동", 10);
+        person[1] = new Student("강감찬", 20);
+
+        for (int i = 0; i < person.length; i++) {
+            person[i].printName();
+        }
+
     }
 }
